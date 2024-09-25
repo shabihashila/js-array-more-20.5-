fe4acedc34d5f29eb8b8a38bef31c441d72f106f
HP <HP@DESKTOP-8E0FCDH>
2024-09-26 01:35:17 +0600

    reverse.js

---
+++ reverse.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const reversed = [];
+// for (let i = 0; i <= numbers.length; i++) {
+//   const num = numbers[i];
+//   reversed.unshift(num);
+// }
+// // const number = numbers.reverse();
+// // ekhane reverse na thakleo savabik onk kico thakte pare
+
+// for (const number of numbers) {
+//   console.log(number);
+// }
+const num = numbers.reverse();
+for (const num of numbers) {
+  console.log(num);
+}
