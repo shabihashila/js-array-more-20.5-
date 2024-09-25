e1b87e0cf612354b4bb979682aea11237b809fa3
HP <HP@DESKTOP-8E0FCDH>
2024-09-26 00:38:43 +0600

    looping.js

---
+++ looping.js
@@ -0,0 +1,24 @@
+// looping technique
+// const friends = ["elon", "bill", "mark", "warn"];
+// // for of use
+// for (const friend of friends) {
+//   //   console.log(friend);
+// }
+// for (let i = 0; i <= friends.length; i++) {
+//   console.log(i);
+//   console.log(friends[i]);
+// }
+const numbers = [
+  34, 65, 46, 25, 66, 45, 34, 85, 67, 63, 24, 45, 33, 22, 55, 6, 4, 55, 6, 65,
+  76, 8, 67,
+];
+
+let i = 0;
+while (i <= numbers.length) {
+  console.log(numbers[i]);
+  i++;
+}
+// for (let i = 0; i <= numbers.length; i++) {
+// //   console.log(i);
+//   console.log(numbers[i]);
+// }
